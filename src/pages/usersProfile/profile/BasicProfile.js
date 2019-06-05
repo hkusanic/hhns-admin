e01cb67407c136c93fc0415ed57e237a28873e32
Shailendra12219 <shailendra@cronj.com>
2019-06-05 19:50:17 +0530

    profile pic binded

--- src/pages/usersProfile/profile/BasicProfile.js
+++ src/pages/usersProfile/profile/BasicProfile.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unused-state */
 import React, { Component, Fragment } from 'react'
 import { Form, Input, Select, Button, Upload } from 'antd'
 import { withRouter } from 'react-router-dom'
@@ -7,10 +6,10 @@ import './BasicProfile.css'
 const FormItem = Form.Item
 const { Option } = Select
 
-const AvatarView = ({ avatar }) => (
+const AvatarView = ({ profilePic }) => (
   <Fragment>
     <div className="avatar">
-      <img src={avatar} alt="avatar" className="avatar_img" />
+      <img src={profilePic} alt="avatar" className="avatar_img" />
     </div>
     <Upload fileList={[]}>
       <div className="button_view">
@@ -41,9 +40,12 @@ class BasicProfile extends Component {
     }
   }
 
-  getAvatarURL = () => {
-    const url = 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
-    return url
+  getProfileUrl = () => {
+    const { user } = this.state
+    if (user.profile_pic === 'Profile pic not available') {
+      return 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+    }
+    return user.profile_pic
   }
 
   render() {
@@ -52,6 +54,12 @@ class BasicProfile extends Component {
     } = this.props
 
     const { user } = this.state
+
+    console.log('user.profile_pic ===>>>', user.profile_pic)
+
+    if (!user) {
+      return <div>Loading...</div>
+    }
 
     return (
       <Form layout="vertical" onSubmit={this.handleSubmit} hideRequiredMark>
@@ -92,12 +100,7 @@ class BasicProfile extends Component {
             </div>
 
             <div className="col-lg-4 imageDiv">
-              <AvatarView avatar={this.getAvatarURL()} />
-              {/* <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br /> */}
+              <AvatarView profilePic={this.getProfileUrl()} />
             </div>
           </div>
 
