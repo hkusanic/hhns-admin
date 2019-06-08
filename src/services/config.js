4df4bd68f35a868d8007eb253704ad7a2afd0606
Shailendra12219 <shailendra@cronj.com>
2019-06-08 11:52:26 +0530

    small changes

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const serverAddress = window.location.protocol + '//' + window.location.hostname
+let serverAddress = window.location.protocol + '//' + window.location.hostname
 
 if (window.location.hostname === 'localhost') {
   serverAddress = serverAddress + ':' + 3000
