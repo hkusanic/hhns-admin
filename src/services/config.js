96af1278a8ba5ed5281c49c43669e98f81becce1
Shailendra12219 <shailendra@cronj.com>
2019-06-08 11:44:41 +0530

    config changes

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,8 @@
 /* eslint-disable */
 const serverAddress = window.location.protocol + '//' + window.location.hostname
 
+if (window.location.hostname === 'localhost') {
+  serverAddress = serverAddress + ':' + 3000
+}
+
 export default serverAddress
